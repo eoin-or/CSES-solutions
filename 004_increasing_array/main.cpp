9235a09c3be4b1e91acd0c170f6ad15ef32f3270
Eoin O'Reilly <eoinoreilly924@gmail.com>
2023-10-19 20:01:30 +0200

    ok i lied. problem 4 solved

--- 004_increasing_array/main.cpp
+++ 004_increasing_array/main.cpp
@@ -6,5 +6,26 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+
+    long long moves = 0;
+    for (int i = 0; i < n; i++) {
+        if (i == 0)
+            continue;
+        if (a[i] >= a[i-1])
+            continue;
+        else {
+            moves += a[i-1] - a[i];
+            a[i] = a[i-1];
+        }
+    }
+
+    cout << moves;
 	return 0;
 }
