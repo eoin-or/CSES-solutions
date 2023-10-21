79c091ea03a48aa7e4c9b8105c56bd7c1d70104a
Eoin O'Reilly <eoinoreilly924@gmail.com>
2023-10-21 09:44:07 +0200

    problem 5 solved

--- 005_permutations/main.cpp
+++ 005_permutations/main.cpp
@@ -3,8 +3,31 @@ using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+    // ios::sync_with_stdio(false);
+    // cin.tie(0);
 
-	return 0;
+    int n;
+    cin >> n;
+
+    if (n == 1)
+    {
+        cout << '1';
+    }
+    if (n < 4)
+        cout << "NO SOLUTION";
+    else if (n == 4)
+        cout << "2 4 1 3\n";
+    else
+    {
+        for (int i = 1; i <= n; i += 2)
+        {
+            cout << i << ' ';
+        }
+        for (int i = 2; i <= n; i += 2)
+        {
+            cout << i << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
 }
